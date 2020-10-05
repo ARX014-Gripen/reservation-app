30bd29d84e9e077bbd2c987f60903053d532c95f
Gripen <konakera@gmail.com>
2020-10-06 00:16:30 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require("./config/dev")
+const config = require("./config")
 const FakeDb = require("./fake-db")
 
 const productRoutes = require("./route/products")
-
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
@@ -13,24 +13,29 @@ mongoose.connect(config.DB_URI, {
   useCreateIndex: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if (process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb()
+      fakeDb.initDb()
+    }
   }
 )
-;
+  ;
 
 const app = express()
 
-app.use('/api/v1/products',productRoutes)
+app.use('/api/v1/products', productRoutes)
 
-// app.get('/products', function(req,res){
-//     res.json({'success':true})
-// })
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || '3001'
 
-app.listen(PORT,function(){
-    console.log('I am running!')
+app.listen(PORT, function () {
+  console.log('I am running!')
 })
 
-// mongodb+srv://test:<password>@cluster0.zc0xp.mongodb.net/<dbname>?retryWrites=true&w=majority
